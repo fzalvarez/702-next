11faaf919a8a71aef5f1e0099eacb6e9e3d828ae
Mark Pineda <mark.pineda@mcets-inc.com>
2024-01-24 18:11:59 -0500

    init i18n

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+import createNextIntlPlugin from 'next-intl/plugin';
+ 
+const withNextIntl = createNextIntlPlugin('./src/i18n.tsx');
+export default withNextIntl();
